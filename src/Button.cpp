796be83fd3c53b04af8b9ef22c63fb62de699a89
loicLE-HENANF <loic.le-henanf@supinfo.com>
2023-11-14 19:18:27 +0100

    button implementation

--- src/Button.cpp
+++ src/Button.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "../headers/Button.h"
+
+void Button::Draw() {
+    GameEngine::DrawRectangle(buttonPos, buttonSize, color);
+}
+
+int Button::CanClick() {
+    return GameEngine::CheckCollisionPointRec(GameEngine::GetMousePosition(),
+                                              buttonPos, buttonSize);
+}
+
+void Button::DetectClick() {
+    if(CanClick()){
+        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+            std::cout << "button pressed." << std::endl;
+        }else if(IsMouseButtonReleased(MOUSE_BUTTON_LEFT)){
+            GameState::currentState = targetState;
+        }
+    }
+}
+
