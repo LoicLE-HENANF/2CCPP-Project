c1e2b9066aaee68a904ca53000eb2d1131cbe4cd
loicLE-HENANF <loic.le-henanf@supinfo.com>
2023-11-15 16:36:27 +0100

    Cmakelist changes + CheckBox.cpp added

---
+++ headers/CheckBox.h
@@ -0,0 +1,14 @@
+//
+// Created by lehen on 15/11/2023.
+//
+
+#ifndef INC_2CCPP_PROJECT_CHECKBOX_H
+#define INC_2CCPP_PROJECT_CHECKBOX_H
+
+
+class CheckBox {
+
+};
+
+
+#endif //INC_2CCPP_PROJECT_CHECKBOX_H
---
+++ src/CheckBox.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lehen on 15/11/2023.
+//
+
+#include "../headers/CheckBox.h"
